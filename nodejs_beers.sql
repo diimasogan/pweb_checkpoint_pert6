52869832a03cb1bc37735fa806a18b84211bad1f
diimasogan <dimasogan231@gmail.com>
2022-12-03 08:28:36 +0700

    first commit

---
+++ nodejs_beers.sql
@@ -0,0 +1,1 @@
+CREATE TABLE `nodejs_beers`.`beers` ( `id` BIGINT NOT NULL AUTO_INCREMENT , `name` VARCHAR(255) NOT NULL , `tagline` VARCHAR(255) NOT NULL , `description` TEXT NOT NULL , `image` VARCHAR(50) NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;
